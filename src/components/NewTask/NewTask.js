6b28b2d7b20644c27007ea3cb65951481578cef5
yurisokolovicz <yurisokolovicz@gmail.com>
2023-03-28 16:45:17 -0300

    adjustments

--- src/components/NewTask/NewTask.js
+++ src/components/NewTask/NewTask.js
@@ -3,47 +3,45 @@ import { useState } from 'react';
 import Section from '../UI/Section';
 import TaskForm from './TaskForm';
 
-const NewTask = (props) => {
-  const [isLoading, setIsLoading] = useState(false);
-  const [error, setError] = useState(null);
+const NewTask = props => {
+    const [isLoading, setIsLoading] = useState(false);
+    const [error, setError] = useState(null);
 
-  const enterTaskHandler = async (taskText) => {
-    setIsLoading(true);
-    setError(null);
-    try {
-      const response = await fetch(
-        'https://react-http-6b4a6.firebaseio.com/tasks.json',
-        {
-          method: 'POST',
-          body: JSON.stringify({ text: taskText }),
-          headers: {
-            'Content-Type': 'application/json',
-          },
+    const enterTaskHandler = async taskText => {
+        setIsLoading(true);
+        setError(null);
+        try {
+            // To store data in Firebase, we need to send a POST request to a specific URL.
+            const response = await fetch('https://react-http-f7e2d-default-rtdb.firebaseio.com/tasks.json', {
+                method: 'POST',
+                body: JSON.stringify({ text: taskText }),
+                headers: {
+                    'Content-Type': 'application/json'
+                }
+            });
+
+            if (!response.ok) {
+                throw new Error('Request failed!');
+            }
+
+            const data = await response.json();
+
+            const generatedId = data.name; // firebase-specific => "name" contains generated id
+            const createdTask = { id: generatedId, text: taskText };
+
+            props.onAddTask(createdTask);
+        } catch (err) {
+            setError(err.message || 'Something went wrong!');
         }
-      );
+        setIsLoading(false);
+    };
 
-      if (!response.ok) {
-        throw new Error('Request failed!');
-      }
-
-      const data = await response.json();
-
-      const generatedId = data.name; // firebase-specific => "name" contains generated id
-      const createdTask = { id: generatedId, text: taskText };
-
-      props.onAddTask(createdTask);
-    } catch (err) {
-      setError(err.message || 'Something went wrong!');
-    }
-    setIsLoading(false);
-  };
-
-  return (
-    <Section>
-      <TaskForm onEnterTask={enterTaskHandler} loading={isLoading} />
-      {error && <p>{error}</p>}
-    </Section>
-  );
+    return (
+        <Section>
+            <TaskForm onEnterTask={enterTaskHandler} loading={isLoading} />
+            {error && <p>{error}</p>}
+        </Section>
+    );
 };
 
 export default NewTask;
