2f92c4deb0121c853791c86b94281908d9823a20
yurisokolovicz <yurisokolovicz@gmail.com>
2023-03-29 10:04:47 -0300

    Using The Custom Hook In More Components

--- src/components/NewTask/NewTask.js
+++ src/components/NewTask/NewTask.js
@@ -1,39 +1,29 @@
-import { useState } from 'react';
-
 import Section from '../UI/Section';
 import TaskForm from './TaskForm';
+import useHttp from '../../hooks/use-http';
 
 const NewTask = props => {
-    const [isLoading, setIsLoading] = useState(false);
-    const [error, setError] = useState(null);
+    const { isLoading, error, sendRequest: sendTaskRequest } = useHttp();
+
+    const createTask = (taskText, taskData) => {
+        const generatedId = taskData.name; // firebase-specific => "name" contains generated id
+        const createdTask = { id: generatedId, text: taskText };
+
+        props.onAddTask(createdTask);
+    };
 
     const enterTaskHandler = async taskText => {
-        setIsLoading(true);
-        setError(null);
-        try {
-            // To store data in Firebase, we need to send a POST request to a specific URL.
-            const response = await fetch('https://react-http-f7e2d-default-rtdb.firebaseio.com/tasks.json', {
+        sendTaskRequest(
+            {
+                url: 'https://react-http-f7e2d-default-rtdb.firebaseio.com/tasks.json',
                 method: 'POST',
-                body: JSON.stringify({ text: taskText }),
+                body: { text: taskText },
                 headers: {
                     'Content-Type': 'application/json'
                 }
-            });
-
-            if (!response.ok) {
-                throw new Error('Request failed!');
-            }
-
-            const data = await response.json();
-
-            const generatedId = data.name; // firebase-specific => "name" contains generated id
-            const createdTask = { id: generatedId, text: taskText };
-
-            props.onAddTask(createdTask);
-        } catch (err) {
-            setError(err.message || 'Something went wrong!');
-        }
-        setIsLoading(false);
+            },
+            createTask.bind(null, taskText)
+        );
     };
 
     return (
